73bbcefa33c241a555ec89cd0c2ae45fbfeacfc9
donaldte <donaldtedom0@gmail.com>
2022-02-09 09:58:28 +0100

    images and forms

--- products/views.py
+++ products/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Products
+from .form import ProductForm
+
+def product_list(request, *args, **kwargs):
+    product = Products.objects.all()
+    context = {
+        'products':product
+    }
+    return render(request, 'products/list.html', context)
+
+def productCreate(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+        messages = "We have receive your product"
+
+
+    return render(request, 'products/create.html', {'form':form, 'message':messages})    
 
 
 
-
-"""
-A---->B------> une instruction | instruction 
-2-2 | *4 ---------->0
-"""
-#ULR et Request
-def home(request, *args, **kwargs):
-    mylist = [45, 66, 676, 67]
-    nom = "donald"
-    return render(request, 'index4.html', {"list":mylist, 'nom':nom})
-
-
-def contact(request):
-    return render(request, 'contact.html')
-
-def blog(request):
-    return HttpResponse("blog page")        
