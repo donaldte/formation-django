746f4099837c5b82d2eaa3f3d3fd28cb44aa2f6d
donaldte <donaldtedom0@gmail.com>
2022-02-12 04:28:48 +0100

    row form

--- products/form.py
+++ products/form.py
@@ -5,3 +5,11 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Products
         fields = ('name', 'description', 'price', 'image', 'slug',)
+
+
+class RowProductForm(forms.Form):
+    name = forms.CharField()
+    description = forms.CharField()
+    price = forms.DecimalField()
+    # image = forms.ImageField()
+    slug = forms.CharField()       
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Products
-from .form import ProductForm
+from .form import ProductForm, RowProductForm
 
 def product_list(request, *args, **kwargs):
     product = Products.objects.all()
@@ -9,15 +9,38 @@ def product_list(request, *args, **kwargs):
     }
     return render(request, 'products/list.html', context)
 
+# def productCreate(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+#         messages = "We have receive your product"
+
+
+#     return render(request, 'products/create.html', {'form':form, 'message':messages}) 
+
+
+# def productCreate(request):
+#     if request.method == "POST":    
+#         name = request.POST.get('name')
+#         description = request.POST.get('description')
+#         price = request.POST.get('price')
+#         image = request.POST.get('image')
+#         slug = request.POST.get('slug')
+#         newProduct = Products.objects.create(name=name, description=description, price=price, image=image, slug=slug)
+#         newProduct.save()
+#         message = "your product was saved successfully"
+#     return render(request, 'products/create.html', {"message":message}) 
+
 def productCreate(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-        messages = "We have receive your product"
-
-
-    return render(request, 'products/create.html', {'form':form, 'message':messages})    
-
-
-
+    form = RowProductForm()
+    message = ''
+    if request.method == "POST":
+        form = RowProductForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            new = Products.objects.create(**form.cleaned_data)
+            new.save()
+            form = RowProductForm()
+            message = 'product was successfully saved'
+    return render(request, 'products/create.html', {'form':form, 'message':message})
